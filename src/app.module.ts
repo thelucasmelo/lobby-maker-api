6cb4f3c4767513f294fcdd5d22f861348cce0241
luckasmelo.dev@gmail.com <luckasmelo.dev@gmail.com>
2025-02-03 22:23:27 -0300

    feat(user): included new endpoints to manager user

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+        type: 'sqlite',
+        database: 'lobby-maker',
+        entities: [User],
+        synchronize: true
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
